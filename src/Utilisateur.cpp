cace212996ed1a2b18b8feb4834c8c8e12e8c545
Emremor5 <110790521+Emremor5@users.noreply.github.com>
2026-05-04 01:04:05 +0000

    Add files via upload

---
+++ src/Utilisateur.cpp
@@ -0,0 +1,26 @@
+#include "Utilisateur.h"
+
+Utilisateur::Utilisateur(string n, int p) {
+    nom = n;
+    points = p;
+}
+
+string Utilisateur::getNom() {
+    return nom;
+}
+
+int Utilisateur::getPoints() {
+    return points;
+}
+
+void Utilisateur::ajouterPoints(int p) {
+    points += p;
+}
+
+bool Utilisateur::utiliserPoints(int p) {
+    if(points >= p) {
+        points -= p;
+        return true;
+    }
+    return false;
+}
