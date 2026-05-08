cd7d27c778065a04c4688bc2db82b7756b72ee3f
Ayibisba <nene23doucoure@gmail.com>
2026-05-08 02:25:06 +0000

    Enhance main function with user name input
    Added user name input and greeting message.

--- src/main.cpp
+++ src/main.cpp
@@ -7,43 +7,47 @@
 #include "../include/Session.h"
 using namespace std;
 
+int main() {
+    string codeUtilisateur;
+    string nomUtilisateur;
 
-
-int main() {string codeUtilisateur;
-    
     cout << "=== BIENVENUE SUR ORANGE SARGAL ===" << endl;
     cout << "Pour acceder au service, tapez #221# : ";
     cin >> codeUtilisateur;
-    
+
     if (verifierAcces(codeUtilisateur)) {
+        cout << "\nComment vous appelez-vous ? ";
+        cin >> nomUtilisateur;
+        cout << "\nBonjour " << nomUtilisateur << " ! Vous avez 200 points Sargal." << endl;
 
-        Utilisateur user("Client Sargal", 200);
-
+        Utilisateur user(nomUtilisateur, 200);
         Catalogue cat;
         cat.ajouterCadeau(Cadeau("Pass Internet 5Go", 50));
         cat.ajouterCadeau(Cadeau("Credit Appel 2000F", 100));
         cat.ajouterCadeau(Cadeau("Smartphone", 500));
-        
+
         int choix;
         do {
             afficherMenu();
             choix = demanderChoix();
-            
+
             if (choix == 1) {
                 gererEchange(user, cat);
-            } 
+            }
             else if (choix == 2) {
-                cout << "\nVos points : " << user.getPoints() << " pts\n";
+                cout << endl << user.getNom() << ", vous avez "
+                     << user.getPoints() << " points." << endl;
             }
             else if (choix == 3) {
-                cout << "Merci ! A bientot sur Orange Sargal." << endl;
+                cout << endl << "Merci " << user.getNom()
+                     << " ! A bientot sur Orange Sargal." << endl;
             }
             else {
                 cout << " Erreur : choisis 1, 2 ou 3" << endl;
             }
-            
-        } while(choix != 3);
+
+        } while (choix != 3);
     }
-    
+
     return 0;
 }
