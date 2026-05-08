dc5f90099ca6759293a53b830d739d3b9bf61d8f
Ayibisba <nene23doucoure@gmail.com>
2026-05-08 03:22:03 +0000

    feat: interface colorée, bannière, clear écran, sous-menu cadeaux

---
+++ include/Couleurs.h
@@ -0,0 +1,13 @@
+#ifndef COULEURS_H
+#define COULEURS_H
+
+#define RESET   "\033[0m"
+#define ORANGE  "\033[38;5;214m"
+#define VERT    "\033[32m"
+#define ROUGE   "\033[31m"
+#define BLANC   "\033[97m"
+#define GRIS    "\033[90m"
+#define JAUNE   "\033[33m"
+#define BOLD    "\033[1m"
+
+#endif
