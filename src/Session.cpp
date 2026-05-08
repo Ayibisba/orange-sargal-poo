3640dbf757b79f7a6894798f74cc9f2838da8fd8
Ayibisba <nene23doucoure@gmail.com>
2026-05-08 02:06:11 +0000

    fix: correction Session.cpp - afficherCadeaux et getCout

--- src/Session.cpp
+++ src/Session.cpp
@@ -3,7 +3,7 @@
 #include "../include/Utilisateur.h" 
 
 void gererEchange(Utilisateur &user, Catalogue &cat) {
-    cat.afficherCatalogue();
+    cat.afficherCadeaux();
     int choix;
     std::cout << "Choisissez un cadeau (0 pour annuler) : ";
     std::cin >> choix;
@@ -12,11 +12,14 @@ void gererEchange(Utilisateur &user, Catalogue &cat) {
         Cadeau c = cat.getCadeau(choix - 1);
         
         // Vérification et mise à jour (Critères 1 & 2)
-        if (user.getPoints() >= c.prixPoints) {
-            user.setPoints(user.getPoints() - c.prixPoints);
-            std::cout << " Succès ! Nouveau solde : " << user.getPoints() << " points.\n";
+        if (user.getPoints() >= c.getCout()) {
+            user.setPoints(user.getPoints() - c.getCout());
+            cout << "Succes ! Cadeau : " << c.getNom() << "\n";
+            cout << "Nouveau solde : " << user.getPoints() << " points.\n";
         } else {
-            std::cout << " Points insuffisants !\n";
+            cout << "Points insuffisants !\n";
         }
+    } else if (choix != 0) {
+        cout << "Choix invalide.\n";
     }
 }
