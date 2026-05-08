879338180e9631d1968d2936d8c8e64b4fc9e8ba
Ayibisba <nene23doucoure@gmail.com>
2026-05-08 02:58:50 +0000

    Enhance gift display with color and formatting
    Updated the display of gifts in the Catalogue class to include color formatting and changed the output format.

--- src/Catalogue.cpp
+++ src/Catalogue.cpp
@@ -1,4 +1,5 @@
 #include "../include/Catalogue.h"
+#include "../include/Couleurs.h"
 #include <iostream>
 using namespace std;
 
@@ -6,11 +7,11 @@ void Catalogue::ajouterCadeau(Cadeau c) {
     cadeaux.push_back(c);
 }
 void Catalogue::afficherCadeaux() {
-    cout << "\n--- Catalogue des cadeaux ---\n";
     for (int i = 0; i < cadeaux.size(); i++) {
-        cout << i+1 << ". "
+        cout << BLANC << "  " << i+1 << ". 🎀 "
              << cadeaux[i].getNom()
-             << " (" << cadeaux[i].getCout() << " points)\n";
+             << GRIS << " (" << cadeaux[i].getCout() << " pts)"
+             << RESET << endl;
     }
 }
 Cadeau Catalogue::getCadeau(int index) { return cadeaux[index]; }
