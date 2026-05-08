91638e61edac36f4e3e510bdac42eae98dd3b447
Ayibisba <nene23doucoure@gmail.com>
2026-05-08 03:00:25 +0000

    Enhance main.cpp with color output and banner
    Updated user interaction with color coding and added banner display.

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,26 @@
 #include "../include/Utilisateur.h"
 #include "../include/Catalogue.h"
 #include "../include/Session.h"
+#include "../include/Couleurs.h"
 using namespace std;
 
 int main() {
     string codeUtilisateur;
     string nomUtilisateur;
 
-    cout << "=== BIENVENUE SUR ORANGE SARGAL ===" << endl;
-    cout << "Pour acceder au service, tapez #221# : ";
+    afficherBanniere();
+
+    cout << ORANGE << "  Pour acceder au service, tapez #221# : " << RESET;
     cin >> codeUtilisateur;
 
     if (verifierAcces(codeUtilisateur)) {
-        cout << "\nComment vous appelez-vous ? ";
+        cout << JAUNE << "\n  Comment vous appelez-vous ? " << RESET;
         cin >> nomUtilisateur;
-        cout << "\nBonjour " << nomUtilisateur << " ! Vous avez 200 points Sargal." << endl;
+        cout << VERT << BOLD << "\n  Bonjour " << nomUtilisateur
+             << " ! Vous avez 200 points Sargal. 🎉" << RESET << endl;
 
         Utilisateur user(nomUtilisateur, 200);
+
         Catalogue cat;
         cat.ajouterCadeau(Cadeau("Pass Internet 5Go", 50));
         cat.ajouterCadeau(Cadeau("Credit Appel 2000F", 100));
@@ -35,18 +39,27 @@ int main() {
                 gererEchange(user, cat);
             }
             else if (choix == 2) {
-                cout << endl << user.getNom() << ", vous avez "
-                     << user.getPoints() << " points." << endl;
+                cout << endl;
+                afficherSeparateur();
+                cout << JAUNE << BOLD << "  💰 " << user.getNom()
+                     << ", vous avez " << user.getPoints()
+                     << " points Sargal." << RESET << endl;
+                afficherSeparateur();
             }
             else if (choix == 3) {
-                cout << endl << "Merci " << user.getNom()
-                     << " ! A bientot sur Orange Sargal." << endl;
+                cout << endl;
+                afficherSeparateur();
+                cout << ORANGE << BOLD << "  Merci " << user.getNom()
+                     << " ! A bientot sur Orange Sargal. 🍊" << RESET << endl;
+                afficherSeparateur();
             }
             else {
-                cout << " Erreur : choisis 1, 2 ou 3" << endl;
+                cout << ROUGE << "\n  Erreur : choisis 1, 2 ou 3" << RESET << endl;
             }
 
         } while (choix != 3);
+    } else {
+        cout << ROUGE << BOLD << "\n  Acces refuse. Verifiez le code." << RESET << endl;
     }
 
     return 0;
